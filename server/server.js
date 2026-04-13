89de3dbd35b97b67658f40fc9d84441111b784b5
Laney Dunker <lddunke@ilstu.edu>
2026-04-13 14:45:41 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import OpenAI from "openai";
 import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
